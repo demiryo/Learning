e383b9b59dd2bc114816b3986e2e2d4d657ae874
omar <omar@demiry.com>
2016-01-31 14:29:40 -0800

    Adding geometry, and rearranging the code

---
+++ ConsoleApplication1/MathFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class MathFunctions
+	{
+		public static void AddHandler()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to add!");
+
+			float first = UserInterface.AskUserForNumber("first");
+			float second = UserInterface.AskUserForNumber("second");
+
+			Console.Write(String.Format("{0} + {1} = {2}", first, second, first + second));
+			Console.ReadKey();
+		}
+		public static void SubtractHandler()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to sub!");
+
+			float first = UserInterface.AskUserForNumber("first");
+			float second = UserInterface.AskUserForNumber("second");
+
+			Console.Write(String.Format("{0} - {1} = {2}", first, second, first - second));
+			Console.ReadKey();
+		}
+		public static void MultiplyHandler()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to times!");
+
+			float first = UserInterface.AskUserForNumber("first");
+			float second = UserInterface.AskUserForNumber("second");
+
+			Console.Write(String.Format("{0} x {1} = {2}", first, second, first * second));
+			Console.ReadKey();
+		}
+		public static void DivideHandler()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to divide!");
+
+			float first = UserInterface.AskUserForNumber("first");
+			float second = UserInterface.AskUserForNumber("second");
+
+			Console.Write(String.Format("{0} / {1} = {2}", first, second, first / second));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ConsoleApplication1/UserInterface.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class UserInterface
+	{
+		static string outputMenu(
+			string[] messages)
+		{
+			Console.Clear();
+			foreach (string message in messages)
+			{
+				Console.WriteLine(message);
+			}
+			Console.WriteLine("select a number and press enter");
+
+			return Console.ReadLine();
+		}
+
+
+		public static float AskUserForNumber(string lable)
+		{
+			bool Error = true;
+			float returnValue = 0;
+			do
+			{
+
+				Console.Write("Enter your " + lable + " number:");
+				string first = Console.ReadLine();
+				// Check if something whent wrong then repeat previous step
+				// If no error set Error = false;
+				Error = !float.TryParse(first, out returnValue);
+			} while (Error);
+			return returnValue;
+		}
+
+		static void mathMenu()
+		{
+			string key;
+			do
+			{
+				key = outputMenu(
+					new string[] {
+				"1. addition",
+				"2. subtraction",
+				"3. multiplacation",
+				"4. divide",
+				"5. exit" });
+
+				if (key == "1")
+				{
+					// call add function
+					MathFunctions.AddHandler();
+				}
+				if (key == "2")
+				{
+					MathFunctions.SubtractHandler();
+				}
+				if (key == "3")
+				{
+					MathFunctions.MultiplyHandler();
+				}
+				if (key == "4")
+				{
+					MathFunctions.DivideHandler();
+				}
+			} while (key != "5");
+		}
+
+		static void geometryMenu()
+		{
+			string key;
+			do
+			{
+				key = outputMenu(
+					new string[] {
+				"1. triangles",
+				"2. rectangle",
+				"3. 3.D. shapes",
+				"4. exit" });
+			} while (key != "4"); // exit if we get a 4
+		}
+
+		public static void mainMenu()
+		{
+			string key;
+			do
+			{
+				key = outputMenu(
+					new string[] {
+				"1. Math",
+				"2. Geometry",
+				"3. exit" });
+				if (key == "1")
+				{
+					mathMenu();
+				}
+				if (key == "2")
+				{
+					geometryMenu();
+				}
+			} while (key != "3");
+		}
+	}
+}
