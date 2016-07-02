340e8458e1af06a9ce28b45ca9153fd063d71626
omar <omar@demiry.com>
2016-07-01 20:25:44 -0700

    first turtle program

---
+++ Python/turtlepogram.py
@@ -0,0 +1,15 @@
+import turtle
+
+fred = turtle.Pen()
+fred.shape("turtle")
+fred.color("green")
+fred.pencolor("brown")
+fred.forward(200)
+fred.circle(100)
+fred.circle(-100)
+fred.forward(200)
+
+
+
+
+turtle.done()
