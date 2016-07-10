62b30a8fd6177967a81be5b641ef2358a47cf549
omar <omar@demiry.com>
2016-07-09 20:38:40 -0700

    Pushing change to continue on other machines

--- Python/turtlepogram.py
+++ Python/turtlepogram.py
@@ -1,15 +1,19 @@
 import turtle
 
-fred = turtle.Pen()
-fred.shape("turtle")
-fred.color("green")
-fred.pencolor("brown")
-fred.forward(200)
-fred.circle(100)
-fred.circle(-100)
-fred.forward(200)
+def HelloWorld(fred):
+    fred.shape("turtle")
+    fred.color("green")
+    fred.pencolor("brown")
+    fred.forward(200)
+    fred.circle(100)
+    fred.circle(-100)
+    fred.forward(200)
 
+def DrawHeart(fred):
+    fred.circle(120, 180, 10)
 
-
-
+myTurtle = turtle.Pen()
+# Call HelloWorld function
+#HelloWorld(myTurtle)
+DrawHeart(myTurtle)
 turtle.done()
