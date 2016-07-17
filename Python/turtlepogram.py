0c526189348df2ad1bff0af6cfc2301922401500
omar <omar@demiry.com>
2016-07-16 22:49:53 -0700

    i finshed the heart! =)

--- Python/turtlepogram.py
+++ Python/turtlepogram.py
@@ -10,7 +10,26 @@ def HelloWorld(fred):
     fred.forward(200)
 
 def DrawHeart(fred):
-    fred.circle(120, 180, 10)
+    fred.shape("turtle")
+    fred.color("green")
+    fred.pencolor("brown")
+    '''
+    fred.penup()
+    fred.right(90)
+    fred.forward(130)
+    fred.right(180)
+    fred.pendown()
+    fred.circle(120, 180, 20)
+    fred.circle(-120, -180, 20)
+    '''
+    fred.right(90)
+    fred.circle(120, -180, 20)
+    fred.circle(-120, 180, 20)
+    fred.right(30)
+    fred.forward(400)
+    fred.right(470)
+    fred.forward(450)
+
 
 myTurtle = turtle.Pen()
 # Call HelloWorld function
