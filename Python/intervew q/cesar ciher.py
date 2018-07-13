4e33b663fe07776c0ad0006fdc9111f9ecce3803
demir_20 <demir_20@mameen-hp.hsd1.wa.comcast.net>
2018-07-12 21:51:49 -0700

    i fixed bug for space by checing if it is a alfabet charicter

--- Python/intervew q/cesar ciher.py
+++ Python/intervew q/cesar ciher.py
@@ -3,25 +3,40 @@
 
 input_string = (raw_input(" input words =  "))
 
+def is_alphabet_character(c):
+    ascii_c = ord(c)
+    ascii_a = ord('A')
+    ascii_z = ord('Z')
+    return ascii_a <= ascii_c and ascii_c <= ascii_z
+
+
 def incrypt(shift, input_string):
     ret = ""
     input_string = input_string.upper()
     for c in input_string:
-        new_ascii = ord(c)+shift
-        if new_ascii > 90:
-            new_ascii = new_ascii - 26
-        ret = ret + chr(new_ascii)
+        if is_alphabet_character(c):
+            new_ascii = ord(c)+shift
+            if new_ascii > 90:
+                new_ascii = new_ascii - 26
+            ret = ret + chr(new_ascii)
+        else:
+            ret = ret + c
     return ret
 def decypt(shift, input_string):
     ret = ""
     input_string = input_string.upper()
     for c in input_string:
-        new_ascii = ord(c) - shift
-        if new_ascii < 65:
-            new_ascii = new_ascii + 26
-        ret = ret + chr(new_ascii)
+        if is_alphabet_character(c):
+            new_ascii = ord(c) - shift
+            if new_ascii < 65:
+                new_ascii = new_ascii + 26
+            ret = ret + chr(new_ascii)
+        else:
+            ret = ret + c
     return ret
 
+for char in input_string.upper():
+    print "is '{}' an alpha {}".format(char, is_alphabet_character(char))
 
 print incrypt(1, input_string)
 print ("decription")
