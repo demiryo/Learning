4c84ae33f17af9d51b953738798cbbd87e19f1c9
demir_20 <demir_20@mameen-hp.hsd1.wa.comcast.net>
2018-07-13 18:27:30 -0700

    i made numbers code it

--- Python/intervew q/cesar ciher.py
+++ Python/intervew q/cesar ciher.py
@@ -10,17 +10,33 @@ def is_alphabet_character(c):
     return ascii_a <= ascii_c and ascii_c <= ascii_z
 
 
+def is_number(c):
+    ascii_c = ord(c)
+    ascii_0 = ord('0')
+    ascii_9 = ord('9')
+    return ascii_0 <= ascii_c and ascii_c <= ascii_9
+
+
+
+
+
 def incrypt(shift, input_string):
     ret = ""
     input_string = input_string.upper()
     for c in input_string:
         if is_alphabet_character(c):
             new_ascii = ord(c)+shift
-            if new_ascii > 90:
+            if new_ascii > ord('Z'):
                 new_ascii = new_ascii - 26
+            ret = ret + chr(new_ascii)
+        elif is_number(c):
+            new_ascii = ord(c) + shift
+            if new_ascii > ord('9'):
+                new_ascii = new_ascii - 10
             ret = ret + chr(new_ascii)
         else:
             ret = ret + c
+
     return ret
 def decypt(shift, input_string):
     ret = ""
@@ -28,16 +44,30 @@ def decypt(shift, input_string):
     for c in input_string:
         if is_alphabet_character(c):
             new_ascii = ord(c) - shift
-            if new_ascii < 65:
+            if new_ascii < ord('A'):
                 new_ascii = new_ascii + 26
             ret = ret + chr(new_ascii)
+        elif is_number(c):
+            new_ascii = ord(c) - shift
+            if new_ascii < ord('0'):
+                new_ascii = new_ascii + 10
+            ret = ret + chr(new_ascii)
+
         else:
             ret = ret + c
+
     return ret
 
-for char in input_string.upper():
-    print "is '{}' an alpha {}".format(char, is_alphabet_character(char))
+
+
+
+
+
+
+#for char in input_string.upper():
+    #print "is '{}' an alpha {}".format(char, is_alphabet_character(char))
 
 print incrypt(1, input_string)
 print ("decription")
 print decypt(1 , incrypt(1, input_string) )
+
