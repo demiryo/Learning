9b2447f4b95d0e1df915f369e73feb6ae659364c
omar <omar@Demiry-AllInOne>
2019-03-31 15:38:15 -0700

    tic tak toe base

---
+++ tic_tak_toe__Omar_demiry/Tic_Tak_toe.cpp
@@ -0,0 +1,24 @@
+// this is tic tak toe by Omar Demiry
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+
+	//declare variables
+	char board[9];
+
+	// assign values to the board 
+	board[0] = '0';
+	board[1] = '1';
+	board[2] = '2';
+	board[3] = '3';
+	board[4] = '4';
+	board[5] = '5';
+	board[6] = '6';
+	board[7] = '7';
+	board[8] = '8';
+
+}
