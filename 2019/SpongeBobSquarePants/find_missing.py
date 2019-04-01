5957322adbcb1e954cdddce07d0d52c6bf24a3e4
omar <omar@Demiry-AllInOne>
2019-03-31 20:51:29 -0700

    made it so it is a funtion

--- 2019/SpongeBobSquarePants/find_missing.py
+++ 2019/SpongeBobSquarePants/find_missing.py
@@ -3,19 +3,24 @@ import xml.etree.ElementTree as ET
 
 SCRIPT_PATH = os.path.dirname(__file__)
 
-file_path = os.path.join(SCRIPT_PATH, "data", "AllEpisodsFromTVDB.xml")
-tree = ET.parse(file_path)
-root = tree.getroot()
+def ReadEpisodeIdFromXml (file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
 
-episodIds = []
-for child in root:
-    seasonNumber = child.find("SeasonNumber")
-    if seasonNumber is None:
-        continue
-    episodeNumber = child.find("EpisodeNumber")
-    # episodeName = child.find("EpisodeName")
-    episodId = "S{0:02}E{1:02}".format(int(seasonNumber.text), int(episodeNumber.text))
-    episodIds.append(episodId)
+    episodIds = []
+    for child in root:
+        seasonNumber = child.find("SeasonNumber")
+        if seasonNumber is None:
+            continue
+        episodeNumber = child.find("EpisodeNumber")
+        # episodeName = child.find("EpisodeName")
+        episodId = "S{0:02}E{1:02}".format(int(seasonNumber.text), int(episodeNumber.text))
+        episodIds.append(episodId)
+    return episodIds
 
-print episodIds
-print len(episodIds)
+###
+
+y = os.path.join(SCRIPT_PATH, "data", "AllEpisodsFromTVDB.xml")
+x = ReadEpisodeIdFromXml(y)
+print x
+print len(x)
